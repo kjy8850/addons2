82887c13b5308b6676a3118f0b71c36282745983
kjy8850 <almexf88@gmail.com>
2025-07-06 10:54:00 +0900

    Update ezville_wallpad.py

--- ezville_wallpad/ezville_wallpad.py
+++ ezville_wallpad/ezville_wallpad.py
@@ -388,7 +388,7 @@ def mqtt_discovery(payload):
     # discovery에 등록
     topic = f"homeassistant/{intg}/ezville_wallpad/{payload['name']}/config"
     logger.info("Add new device: %s", topic)
-    mqtt.publish(topic, json.dumps(payload), retain=True)
+    mqtt.publish(topic, json.dumps(payload))
 
 
 # KTDO: 수정 완료
